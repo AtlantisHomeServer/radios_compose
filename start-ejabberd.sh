02a482c17100bdec675f05be378b6b1afb1e3b6b
ktz <knightpop@gmail.com>
2017-01-24 01:35:03 +0900

    add mysql

--- start-ejabberd.sh
+++ start-ejabberd.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+docker stop ejabberd hadoop
 docker rm ejabberd hadoop
 docker-compose up -d
-sleep 5
+sleep 10
 docker exec mysql bash -c "mysql ejabberd < /root/mysql.sql"
 docker exec ejabberd bash -c "ejabberdctl register admin localhost 123"
