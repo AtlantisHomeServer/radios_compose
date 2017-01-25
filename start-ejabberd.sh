f197e1e92264d9fac298911277c97b9f41216ab5
ktz <ktz@kakaoui-MacBook-Air-6.local>
2017-01-26 05:11:38 +0900

    atlantis-ejabberd-docker version up

--- start-ejabberd.sh
+++ start-ejabberd.sh
@@ -3,6 +3,6 @@
 docker stop ejabberd mysql
 docker rm ejabberd mysql
 docker-compose up -d
-sleep 10
+sleep 20
 docker exec mysql bash -c "mysql ejabberd < /root/mysql.sql"
 docker exec ejabberd bash -c "ejabberdctl register admin localhost 123"
